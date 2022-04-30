8a9d058113613ac04ab6683e9418b479203caeb0
WODweqFS <prohnovigor11@gmail.com>
2022-04-30 12:30:50 +0300

    Initial commit Configuration of Application

---
+++ src/main/java/com/prokhnov/spring_security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.prokhnov.spring_security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
